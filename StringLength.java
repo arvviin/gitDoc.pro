53485f55310bff5007daf3fbda6be5bf7c70cd0a
Aravindh <dragochintu@gmail.com>
2025-12-31 14:11:28 +0530

    first commit

---
+++ StringLength.java
@@ -0,0 +1,10 @@
+public class StringLength {
+    public static void main(String[] args) {
+
+        String greeting = "hello world";
+        System.out.println("string: " + greeting);
+
+        int length = greeting.length();
+        System.out.println("length:" + length);
+    }
+}
