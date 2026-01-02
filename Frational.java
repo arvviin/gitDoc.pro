3afe2cc467b5773c5577e2d57f7e191b2fae0fda
Aravindh <dragochintu@gmail.com>
2026-01-02 19:45:14 +0530

    added new file

---
+++ Frational.java
@@ -0,0 +1,12 @@
+public class Frational {
+    public static void main(String[] args) {
+        int n = 7;
+        int k = 1;
+        for (int i = 1; i <= 7; i++) {
+            k = k * i;
+            System.out.println("frational of " + n + "is the" + k);
+        }
+
+    }
+
+}
