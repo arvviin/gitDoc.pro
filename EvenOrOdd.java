a7c22e72dbfa1495376bcfeede3d28580efd87b2
Aravindh <dragochintu@gmail.com>
2026-01-02 13:11:25 +0530

    new file added

---
+++ EvenOrOdd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class EvenOrOdd {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+
+        int num = sc.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("The number is Even num " + num);
+        } else
+            System.out.println("The number is Odd number " + num);
+    }
+}
