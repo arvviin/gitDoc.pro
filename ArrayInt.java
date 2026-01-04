9211fb1eb89ff576bfecd39cf28920a480bbb934
Aravindh <dragochintu@gmail.com>
2026-01-04 12:20:55 +0530

    added new file

---
+++ ArrayInt.java
@@ -0,0 +1,12 @@
+public class ArrayInt {
+    public static void main(String[] args) {
+        int[] num = new int[5];
+        num[0] = 10;
+        num[1] = 21;
+        num[2] = 30;
+        num[3] = 40;
+        num[4] = 50;
+        System.out.println(num[4]);
+    }
+
+}
