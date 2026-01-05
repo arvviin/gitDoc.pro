4e5abad0150541ca973d396ea07c1b0e5330e8f1
Aravindh <dragochintu@gmail.com>
2026-01-05 21:35:08 +0530

    added new file

---
+++ ArraySmallestElement.java
@@ -0,0 +1,18 @@
+public class ArraySmallestElement {
+
+    public static void main(String[] args) {
+        int[] arr = { 7, 3, 1, 4, 5 };
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+
+                min = arr[i];
+
+            }
+            System.out.println(i + "value" + min + "minvalue");
+
+        }
+        System.out.println("smallest number is" + min);
+    }
+
+}
