ac2c68f1f3c779696ccefbf8f5e43808330b4b5c
Aravindh <dragochintu@gmail.com>
2026-01-05 17:07:50 +0530

    added new fie

---
+++ Array.java
@@ -0,0 +1,9 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] arr = { 20, 30, 40, 40 };
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println((i));
+        }
+
+    }
+}
