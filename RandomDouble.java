4d2945501c8b0a3c322dfc2104d3ee0e99f92527
aravind <dragochintu@gmail.com>
2026-01-07 15:50:02 +0530

    added new file

---
+++ RandomDouble.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class RandomDouble {
+    public static void main(String[] args) {
+        Random random = new Random();
+        double randomDouble = random.nextDouble();
+        System.out.println("your random double is: " + randomDouble);
+    }
+
+}
