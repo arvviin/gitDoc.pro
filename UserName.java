5cd6b12c9674a7723d47bab5a9682e324180bd8f
aravind <dragochintu@gmail.com>
2026-01-08 19:52:06 +0530

    added new file

---
+++ UserName.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class UserName {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = " ";
+        while (name.isBlank()) {
+            System.out.println("Enter your name");
+            name = Scanner.nextline();
+        }
+        System.out.println("Hello" + name);
+    }
+}
