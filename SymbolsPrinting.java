854a060806e51c2cb3533815a1690a7e0b0d6cdc
aravind <dragochintu@gmail.com>
2026-01-10 13:41:14 +0530

    added new file into github

---
+++ SymbolsPrinting.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SymbolsPrinting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int column;
+        int rows;
+        String Symbol = "";
+        System.out.println("Enter number of rows");
+        rows = sc.nextInt();
+        System.out.println("Enter number of column");
+        column = sc.nextInt();
+        System.out.println("Enter your Symbol");
+        Symbol = sc.next();
+
+        for (int i = 0; i <= rows; i++) {
+            System.out.println();
+            for (int j = 0; j <= column; j++) {
+                System.out.print(Symbol);
+            }
+        }
+        sc.close();
+    }
+
+}
