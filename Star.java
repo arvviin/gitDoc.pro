52beb38f77fc1262fce5916b71075554a555c779
aravind <dragochintu@gmail.com>
2026-01-12 11:40:18 +0530

    Added new file

---
+++ Star.java
@@ -0,0 +1,8 @@
+public class Star {
+    public static void main(String[] args) {
+        int i = 1;
+        for (int n = 1; n <= 4; n++) {
+            System.out.println("*");
+        }
+    }
+}
