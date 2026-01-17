b318c1ce122bf2a00f5063dd355e187e536f31bd
aravind <dragochintu@gmail.com>
2026-01-17 17:24:58 +0530

    added new file

---
+++ AverageSum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class AverageSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Three nums");
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int avg = (a + b + c) / 3;
+        System.out.println("Your average :" + avg);
+    }
+}
