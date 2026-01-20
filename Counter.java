b028578f48aa177e5c16f519ba106aa798e78d47
aravind <dragochintu@gmail.com>
2026-01-20 15:53:03 +0530

    added new file

---
+++ Counter.java
@@ -0,0 +1,8 @@
+public class Counter {
+    public static void main(String[] args) {
+        int counter = 1;
+        for (counter = 1; counter <= 10; counter++) {
+            System.out.print(counter + " ");
+        }
+    }
+}
