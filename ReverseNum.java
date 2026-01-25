4a61ef7354fcb808ddf4062d1bbf5f6178fbb466
aravind <dragochintu@gmail.com>
2026-01-25 09:00:55 +0530

    added new to github

---
+++ ReverseNum.java
@@ -0,0 +1,10 @@
+public class ReverseNum {
+    public static void main(String[] args) {
+        int n = 100;
+        while (n > 0) {
+            int lastdigit = n % 10;
+            System.out.print(lastdigit);
+            n = n / 10;
+        }
+    }
+}
