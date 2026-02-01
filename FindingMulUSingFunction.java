db88810029353221b01703bd1ad1ffb2a3c5adcb
aravind <dragochintu@gmail.com>
2026-02-01 21:29:36 +0530

    added new file

---
+++ FindingMulUSingFunction.java
@@ -0,0 +1,10 @@
+public class FindingMulUSingFunction {
+    public static void multiplication(int a, int b) {
+        int mul = a * b;
+        System.out.println("multipication of a " + a + "and b " + b + "is " + mul);
+    }
+
+    public static void main(String[] args) {
+        multiplication(10, 30);
+    }
+}
