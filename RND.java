08cacf7a78abb4426c09dc267d1c95dd0ea58e82
aravind <dragochintu@gmail.com>
2026-02-01 21:52:02 +0530

    added file

---
+++ RND.java
@@ -0,0 +1,9 @@
+public class RND {
+    public static void main(String[] args) {
+        int n = 2;
+        int m = 3;
+        int o = 4;
+        int formula = n * (m + o) / n + o - n % n;
+        System.out.println(formula);
+    }
+}
