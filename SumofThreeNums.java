28246d29cfd42a38dcd9324dd0c06206ea4724d0
aravind <dragochintu@gmail.com>
2026-02-01 21:30:57 +0530

    added new file

---
+++ SumofThreeNums.java
@@ -0,0 +1,9 @@
+public class SumofThreeNums {
+    public static void main(String args[]) {
+        int A = 10;
+        int B = 10;
+        int C = 10;
+        int sum = A + B + C;
+        System.out.println(sum);
+    }
+}
