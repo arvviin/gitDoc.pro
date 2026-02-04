552bf32295fa132e32851e67734d11edc0d0656c
aravind <dragochintu@gmail.com>
2026-02-04 07:36:05 +0530

    added new into git hub

---
+++ UsingPara.java
@@ -0,0 +1,9 @@
+public class UsingPara {
+    public static void sum(int a, int b) {
+        System.out.println("sum of a and b is " + (a + b));
+    }
+
+    public static void main(String[] args) {
+        sum(10, 20);
+    }
+}
