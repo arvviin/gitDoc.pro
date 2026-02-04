e71b7f995cc96b0358a3b8a12e442b03931bf83b
aravind <dragochintu@gmail.com>
2026-02-04 09:49:19 +0530

    added new file

---
+++ Pattern.java
@@ -0,0 +1,18 @@
+public class Pattern {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 1; i <= n; i++) {
+
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Stars
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
