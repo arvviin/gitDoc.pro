fc2a2ad01978c10c10275288362b338d4da6feb0
aravind <dragochintu@gmail.com>
2026-02-08 16:51:33 +0530

    updated som changes

--- AreaOfCircle.java
+++ AreaOfCircle.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 
 public class AreaOfCircle {
     public static void main(String[] args) {
+        // first taking input from the user
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Radius of Circle");
         Double Radius = sc.nextDouble();
+
+        // here we applyed circle formula
         double area = Math.PI * Radius * Radius;
         System.out.println("Area of circle=" + area);
         sc.close();
