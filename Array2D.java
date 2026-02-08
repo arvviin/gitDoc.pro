329d992eee38fda2526e7595d5776d52568958b0
aravind <dragochintu@gmail.com>
2026-02-08 16:52:58 +0530

    updated some changes

--- Array2D.java
+++ Array2D.java
@@ -2,8 +2,16 @@ import java.util.ArrayList;
 
 public class Array2D {
     public static void main(String[] args) {
-        ArrayList<ArrayList<String>> storeroomlist = new ArrayList();
+        /*
+         * here storeroom list is call method
+         * 
+         * here you can use whenever are you this method
+         */
 
+        ArrayList<ArrayList<String>> storeroomlist = new ArrayList();
+        // it is also another function method (when you call this method
+
+        // inside the elements get displayed on screen)
         ArrayList<String> SportcarList = new ArrayList();
         SportcarList.add("Bmw");
         SportcarList.add("Benz");
@@ -22,7 +30,7 @@ public class Array2D {
         storeroomlist.add(FamouscarList);
         storeroomlist.add(NormalList);
 
-        System.out.println(storeroomlist);
+        System.out.println(storeroomlist);// you called the main method so everything get displayed
 
     }
 
