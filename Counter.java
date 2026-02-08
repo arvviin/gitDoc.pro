59af42519121a3d063db8a4e1e4a9022a33f5ad7
aravind <dragochintu@gmail.com>
2026-02-08 17:06:52 +0530

    updated new changes

--- Counter.java
+++ Counter.java
@@ -1,8 +1,9 @@
 public class Counter {
     public static void main(String[] args) {
-        int counter = 1;
+
+        int counter = 1; // counter machine-counting nums
         for (counter = 1; counter <= 10; counter++) {
-            System.out.print(counter + " ");
+            System.out.print(counter + " "); // counter+space
         }
     }
 }
