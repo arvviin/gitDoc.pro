28e0e2401a16b6d563c1f6b52ece423b614d5fe8
aravind <dragochintu@gmail.com>
2026-02-08 17:01:55 +0530

    Updated some changes

--- BasicLoops.java
+++ BasicLoops.java
@@ -1,6 +1,6 @@
 public class BasicLoops {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 5; i++) {// in this loop its runs 1to5 times "hello world"
             System.out.println("Hello World");
         }
     }
