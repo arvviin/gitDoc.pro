f33682768f14c3b36aa4e3e7d1ff35900360fe27
aravind <dragochintu@gmail.com>
2026-02-08 16:52:09 +0530

    updated file

--- Array.java
+++ Array.java
@@ -1,5 +1,7 @@
 public class Array {
     public static void main(String[] args) {
+        // here we added some elements in a array
+
         int[] arr = { 20, 30, 40, 40 };
         for (int i = 0; i < arr.length; i++) {
             System.out.println((i));
