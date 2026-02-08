63a0250d87c2ac2ffd24ade8cb8d1e68e01eca9a
aravind <dragochintu@gmail.com>
2026-02-08 16:59:51 +0530

    Updated some changes

--- ArraySmallestElement.java
+++ ArraySmallestElement.java
@@ -1,8 +1,9 @@
 public class ArraySmallestElement {
 
     public static void main(String[] args) {
+
         int[] arr = { 7, 3, 1, 4, 5 };
-        int min = arr[0];
+        int min = arr[0];// min = minimum value
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] < min) {
 
