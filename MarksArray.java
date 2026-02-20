cc56b09548e5a7c90934ea1316d371a6ef720b8d
aravind <dragochintu@gmail.com>
2026-02-20 18:32:08 +0530

    added new file

---
+++ MarksArray.java
@@ -0,0 +1,17 @@
+public class MarksArray {
+    public static void main(String[] args) {
+        // type[]ArrayName = new type[size]
+        // type arrayname[] ={};
+
+        int[] marks = new int[5];
+        marks[0] = 97;
+        marks[1] = 88;
+        marks[2] = 87;
+        marks[3] = 34;
+        marks[4] = 45;
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(marks[i]);
+        }
+
+    }
+}
